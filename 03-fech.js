adb3d8abffc180dcde1fe8661da745486a0e5abe
Your Name <you@example.com>
2023-05-10 01:35:09 -0500

    09-05-23 cambios

--- 03-fech.js
+++ 03-fech.js
@@ -1,6 +1,7 @@
 
 const resp = fetch('https://reqres.in/api/users?page=1');
 
+
 resp.then(response=> response.json())
 .then((json)=>{
     console.log(json.data);
@@ -10,17 +11,21 @@ resp.then(response=> response.json())
     json.data.forEach(element => {
         console.log(element);
 
-        let htmlCard = `<div class="col-4"><div class="card">
-        <div class="card-header">
-        Featured
-        </div>
-        <div class="card-body">
-          <h5 class="card-title"> Correo Electrónico </h5>
-          <p class="card-text"> Nombre </p>
-          <p class="card-text"> Apellido </p>
-          <a href="#" class="btn btn-info">Detalles</a>
-        </div>
-        <div></div>`
+        let htmlCard = 
+        `<div class="col-4"> 
+          <div class="card">
+            <div class="card-header">
+            Featured
+            </div>
+              <div class="card-body">
+                <h5 class="card-title">${element.id}</h5>
+                <p class="card-text">${element.email}</p>
+                <p class="card-text">${element.first_name}</p>
+                <p class="card-text">${element.last_name}</p>
+                  <a href="#" class="btn btn-info">Detalles</a>
+              </div>
+              </div>
+        </div>`
         htmlX=htmlX+htmlCard;
     });
     content.innerHTML=htmlX;
